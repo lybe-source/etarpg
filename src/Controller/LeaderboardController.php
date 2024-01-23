45dcd18ce46cafe1c51927d3df12a79eb0d16001
Lybe <s3iiryo@live.be>
2024-01-23 19:31:52 +0100

    Feat: Beginning of the leaderboard

--- src/Controller/LeaderboardController.php
+++ src/Controller/LeaderboardController.php
@@ -2,22 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Inventory;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/leaderboard', name: 'classement_')]
 class LeaderboardController extends AbstractController
 {
+    private $security;
+    private $em;
     private $cssClass = "leaderboard";
+
+    public function __construct(Security $security, EntityManagerInterface $em)
+    {
+        $this->security = $security;
+        $this->em = $em;
+    }
 
     #[Route('/', name: 'index')]
     public function index (): Response
     {
-        $perso = "Perso";
+        $user = $this->security->getUser();
+
+        // $totalScore = $this->em->getRepository(Inventory::class)->totalScoreQuery($user);
+        // $totalScores = $this->em->getRepository(Inventory::class)->totalScoreAllUser();
+        $usersWithScores = $this->em->getRepository(Inventory::class)->getAllUsersWithScores($user);
+
+        // dd($totalScores);
 
         return $this->render('leaderboard/index.html.twig', [
-            'perso' => $perso,
+            // 'totalScore' => $totalScore,
+            // 'totalScores' => $totalScores,
+            'usersWithScores' => $usersWithScores,
             'cssClass' => $this->cssClass,
         ]);
     }
--- src/Repository/InventoryRepository.php
+++ src/Repository/InventoryRepository.php
@@ -5,8 +5,8 @@ namespace App\Repository;
 use App\Entity\Inventory;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\SecurityBundle\Security;
 
 /**
  * @extends ServiceEntityRepository<Inventory>
@@ -18,14 +18,55 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class InventoryRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    private $security;
+
+    public function __construct(ManagerRegistry $registry, Security $security)
     {
         parent::__construct($registry, Inventory::class);
+        $this->security = $security;
     }
 
-    // Cumulated score item to User
-    public function totalScoreQuery()
+    public function totalScoreQuery(User $user): ?int
     {
-        
+        $inventory = $this->findOneBy(['user' => $user]);
+
+        if (!$inventory) {
+            return null;
+        }
+
+        $items = $inventory->getInventoryItems();
+
+        $totalScore = 0;
+
+        foreach ($items as $item) {
+            $totalScore += $item->getItems()->getScore();
+        }
+
+        return $totalScore;
+    }
+
+    public function totalScoreAllUser(): array
+    {
+        $users = $this->_em->getRepository(User::class)->findAll();
+        $totalScores = [];
+
+        foreach ($users as $user) {
+            $totalScore = $this->totalScoreQuery($user);
+            $totalScores[$user->getId()] = $totalScore;
+        }
+
+        return $totalScores;
+    }
+
+    public function getAllUsersWithScores(User $currentUser): array
+    {
+        $query = $this->createQueryBuilder('i')
+            ->select('u.username as username, i.totalScore as score')
+            ->leftJoin('i.user', 'u')
+            ->orderBy('CASE WHEN u.id = :userId THEN 1 ELSE 2 END', 'ASC')
+            ->addOrderBy('i.totalScore', 'ASC')
+            ->setParameter('userId', $currentUser->getId());
+
+        return $query->getQuery()->getResult();
     }
 }
